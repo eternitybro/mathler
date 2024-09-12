8980db03d7e91de15d368856e1efea4138a177c9
Rob Gungor <rob@robgungor.com>
2024-09-12 00:38:21 -0700

    fooling the builder

--- src/app/api/daily/route.ts
+++ src/app/api/daily/route.ts
@@ -1,3 +1,4 @@
-export { GET } from "@/integrations/puzzle/api/daily"
+// export { GET } from "@/integrations/puzzle/api/daily"
 
-export const runtime = "edge"
+// export const runtime = "edge"
+export {}
